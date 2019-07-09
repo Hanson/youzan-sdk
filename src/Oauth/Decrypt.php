95c610c56135b19898769b7001f6486be2d4da68
Hanson <h@hanc.cc>
2019-07-09 19:49:19 +0800

    解密消息

--- src/Oauth/Decrypt.php
+++ src/Oauth/Decrypt.php
@@ -6,81 +6,16 @@ use Hanson\Youzan\Youzan;
 
 class Decrypt
 {
-    private $hex_iv = '00000000000000000000000000000000'; # converted JAVA byte code in to HEX and placed it here
-
     private $app;
 
     public function __construct(Youzan $app)
     {
         $this->app = $app;
-        $this->key = hash('sha256', $this->app->getConfig()['client_secret'], true);
     }
 
-    public function encrypt($input)
+    public function decrypt($data)
     {
-        $data = openssl_encrypt($input, 'AES-256-CBC', $this->key, OPENSSL_RAW_DATA, $this->hexToStr($this->hex_iv));
-        $data = base64_encode($data);
-        return $data;
-    }
-
-    public function decrypt($input)
-    {
-        $decrypted = openssl_decrypt(base64_decode($input), 'AES-256-CBC', $this->key, OPENSSL_RAW_DATA, $this->hexToStr($this->hex_iv));
-        return $decrypted;
-    }
-
-    /*
-      For PKCS7 padding
-     */
-
-    private function addpadding($string, $blocksize = 16) {
-
-        $len = strlen($string);
-
-        $pad = $blocksize - ($len % $blocksize);
-
-        $string .= str_repeat(chr($pad), $pad);
-
-        return $string;
-
-    }
-
-    private function strippadding($string) {
-
-        $slast = ord(substr($string, -1));
-
-        $slastc = chr($slast);
-
-        $pcheck = substr($string, -$slast);
-
-        if (preg_match("/$slastc{" . $slast . "}/", $string)) {
-
-            $string = substr($string, 0, strlen($string) - $slast);
-
-            return $string;
-
-        } else {
-
-            return false;
-
-        }
-
-    }
-
-    function hexToStr($hex)
-    {
-
-        $string='';
-
-        for ($i=0; $i < strlen($hex)-1; $i+=2)
-
-        {
-
-            $string .= chr(hexdec($hex[$i].$hex[$i+1]));
-
-        }
-
-        return $string;
+        return openssl_decrypt(urldecode(urldecode($data)), 'AES-128-CBC', substr($this->app->getConfig()['client_secret'], 0, 16), null, '0102030405060708');
     }
 
 }
